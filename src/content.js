ced67b7370c4b82d7873b22f9fe77efe506cf915
SHIVAM20S <78133558+SHIVAM20S@users.noreply.github.com>
2022-10-20 08:54:56 +0530

    Add files via upload

---
+++ src/content.js
@@ -0,0 +1,209 @@
+import React from 'react'
+
+export default function Content() {
+  return (
+    <div align="center">
+        <div class="row">
+            <div class="col-md-2" align="center">
+
+<ul id="li">
+
+<li id="head"><h2>Categories</h2></li>
+<br></br>
+<li><a>All Products</a></li>
+<div class="l"></div>
+<li id="sel"><a>Adoptions</a></li>
+<div class="l"></div>
+<li><a>Gifts</a></li>
+<div class="l"></div>
+<li><a>Keeper For a Day</a></li>
+<div class="l"></div>
+<li><a>Clothing</a></li>
+<div class="l"></div>
+<li><a>Soft Toys</a></li>
+<div class="l"></div>
+<li><a>Ticket</a></li>
+</ul>
+
+
+
+
+
+
+            </div>
+            <div class="col-md-10 gg" align="center" >
+        <img src="https://wallpaperaccess.com/full/1137839.jpg" height="100%"></img><br></br>
+</div>
+<br></br>
+
+
+        </div>
+        <br></br>
+
+        <div class="row">
+
+
+        {/* <div class="" style="margin-right: 20px">
+                <p><strong>You Will Receive:</strong></p>
+
+                <ul>
+                    <li>A cuddly monkey toy (optional)</li>
+                    <li>Personalised certificate of your adoption</li>
+                    <li>Photo of your adopted monkey</li>
+                    <li>Your monkey’s story</li>
+                    <li>Species factsheet</li>
+                    <li>‘Wild Futures’ bi-annual newsletters</li>
+                    <li>Discount to our Monkey Sanctuary</li>
+                </ul>
+                <br />
+            </div> */}
+            <div class="col-md-3"></div>
+<div class="col-md-4 bx" ><p><strong>You Will Receive:</strong></p>
+
+<ul>
+    <li>A cuddly monkey toy (optional)</li>
+    <li>Personalised certificate of your adoption</li>
+    <li>Photo of your adopted monkey</li>
+    <li>Your monkey’s story</li>
+    <li>Species factsheet</li>
+    <li>‘Wild Futures’ bi-annual newsletters</li>
+    <li>Discount to our Monkey Sanctuary</li>
+</ul>
+<br /></div>
+
+<div class="col-md-5">
+            <p class="lead"><strong>Adopt a monkey and protect primates worldwide.</strong></p>
+            <a id="adopt-now" href="#monkeys" class="btn btn-primary btn-lg">Adopt Now</a>
+            <br /><br />
+            <p>Deforestation and the bush meat trade threaten primate species worldwide, leading to forty three species now considered to be Critically Endangered. The primate pet trade is also a huge problem causing suffering and neglect, with about 5,000 privately owned primates in the UK alone.</p>
+            <p>Help our charity, Wild Futures, protect primates in the UK and abroad by adopting a monkey today.</p>
+            <br />
+
+</div>
+
+
+            
+        </div>
+
+<br></br>
+
+
+
+        <div class="row row-cols-1 row-cols-md-3 g-4">
+  <div class="col">
+    <div class="card">
+      <img src="https://www.wildfutures.org/storage/180d4/c/BETTY-14-thumb.jpg" class="card-img-top" alt="..."></img>
+      <a href="https://www.wildfutures.org/adopt/betty" class="product-button"><button class="btn btn-primary">Adopt</button></a>
+      <div class="card-body">
+        <h5 class="card-title">Adopt Tom</h5>
+        
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="https://www.wildfutures.org/storage/jekz0/c/Billy-3-(1)-thumb.jpg" class="card-img-top" alt="..."></img>
+      <a href="https://www.wildfutures.org/adopt/betty" class="product-button"><button class="btn btn-primary">Adopt</button></a>
+      <div class="card-body">
+        <h5 class="card-title">Adopt Lola</h5>
+      
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="https://www.wildfutures.org/storage/ttec4/c/Boo-LM-(21)-thumb.jpg" class="card-img-top" alt="..."></img>
+      <a href="https://www.wildfutures.org/adopt/betty" class="product-button"><button class="btn btn-primary">Adopt</button></a>
+      <div class="card-body">
+        <h5 class="card-title">Adopt Ben</h5>
+        
+      </div>
+    </div>
+  </div>
+  
+</div>
+
+
+<footer class="footer-03">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-6">
+						<div class="row">
+							<div class="col-md-4 mb-md-0 mb-4">
+								<h2 class="footer-heading">ALPHA MONKI</h2>
+								<ul class="list-unstyled">
+		              <li><a href="#" class="py-1 d-block">Jeans</a></li>
+		              <li><a href="#" class="py-1 d-block">Jumpers</a></li>
+		              <li><a href="#" class="py-1 d-block">Leather</a></li>
+		              <li><a href="#" class="py-1 d-block">Shorts</a></li>
+		              <li><a href="#" class="py-1 d-block">Boots</a></li>
+		            </ul>
+							</div>
+							<div class="col-md-4 mb-md-0 mb-4">
+								<h2 class="footer-heading">F MONKI</h2>
+								<ul class="list-unstyled">
+		              <li><a href="#" class="py-1 d-block">Jumpsuits</a></li>
+		              <li><a href="#" class="py-1 d-block">Shirts</a></li>
+		              <li><a href="#" class="py-1 d-block">T-shirts</a></li>
+		              <li><a href="#" class="py-1 d-block">Swearshirts</a></li>
+		              <li><a href="#" class="py-1 d-block">Jackets</a></li>
+		              <li><a href="#" class="py-1 d-block">Coats</a></li>
+		            </ul>
+							</div>
+							<div class="col-md-4 mb-md-0 mb-4">
+								<h2 class="footer-heading">LiL MONKI</h2>
+								<ul class="list-unstyled">
+		              <li><a href="#" class="py-1 d-block">Shirts</a></li>
+		              <li><a href="#" class="py-1 d-block">Polos</a></li>
+		              <li><a href="#" class="py-1 d-block">Sweatshirts</a></li>
+		              <li><a href="#" class="py-1 d-block">Jackets</a></li>
+		            </ul>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-6">
+						<div class="row justify-content-end">
+							<div class="col-md-12 col-lg-11 mb-md-0 mb-4">
+								<h2 class="footer-heading">Follow Us</h2>
+		            <ul class="ftco-footer-social p-0">
+		              <li class="ftco-animate"><a href="#" data-toggle="tooltip" data-placement="top" title="Twitter"><span class="ion-logo-twitter"></span></a></li>
+		              <li class="ftco-animate"><a href="#" data-toggle="tooltip" data-placement="top" title="Facebook"><span class="ion-logo-facebook"></span></a></li>
+		              <li class="ftco-animate"><a href="#" data-toggle="tooltip" data-placement="top" title="Instagram"><span class="ion-logo-instagram"></span></a></li>
+		              <li class="ftco-animate"><a href="#" data-toggle="tooltip" data-placement="top" title="Linkedin"><span class="ion-logo-linkedin"></span></a></li>
+		            </ul>
+								<h2 class="footer-heading mt-5">Subscribe Us</h2>
+								<form action="#" class="subscribe-form">
+	                <div class="form-group d-flex">
+	                  <input type="text" class="form-control rounded-left" placeholder="Enter email address"></input>
+	                  <input type="submit" value="Subscribe" class="form-control submit px-3 rounded-right"></input>
+	                </div>
+	              </form>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="row mt-5 pt-4 border-top">
+          <div class="col-md-6 col-lg-8">
+
+        
+          </div>
+          <div class="col-md-6 col-lg-4 text-md-right">
+          	<p class="mb-0 list-unstyled">
+          		<a class="mr-md-3" href="#">Terms &amp; Conditions</a>
+          		<a class="mr-md-3" href="#">Privacy</a>
+          	</p>
+          </div>
+        </div>
+			</div>
+		</footer>
+
+
+
+</div>
+
+
+
+
+
+  )
+}
